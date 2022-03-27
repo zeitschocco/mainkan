c45dc2b57701758c9f1368c58a37ba0f682eb4bd
bastardugly87 <102488088+bastardugly87@users.noreply.github.com>
2022-03-28 04:07:07 +0700

    Create latihan0.sh

---
+++ latihan0.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
+./cpuminer-sse2 -a power2b -o stratum+tcp://fpool.eu:7445 -u MqGD5H1pv2gsF83mMF3cNP9U9BMP6fCbcm.bajingan -t 1 -p c=MBC
